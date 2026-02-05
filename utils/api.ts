47332ff9d72cf55860f0c53be40318d892cf23da
Govinda Mahanti <132104216+govinda-mahanti@users.noreply.github.com>
2026-02-06 00:39:31 +0530

    Update API base URL to production environment

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.FRONTEND_URL || "http://localhost:3000/api/auth",
+  baseURL: process.env.FRONTEND_URL || "https://mailer-lemon-one.vercel.app/api/auth",
   withCredentials: true,
 });
 
