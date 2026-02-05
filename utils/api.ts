a8ebb75cf5578f4f075d25ef78607a06f5c9e79a
Govinda Mahanti <132104216+govinda-mahanti@users.noreply.github.com>
2026-02-06 00:49:40 +0530

    Update API base URL for authentication

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.FRONTEND_URL || "https://mailer-lemon-one.vercel.app/api/auth",
+  baseURL: process.env.FRONTEND_URL || "https://mailer-three-blue.vercel.app/api/auth",
   withCredentials: true,
 });
 
